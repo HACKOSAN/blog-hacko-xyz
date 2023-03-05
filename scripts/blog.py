092cbfe8a9a7b57804be82df8400527b4010cd00
HACKOSAN <mmuayyed9@gmail.com>
2023-03-06 01:10:57 +0300

    meta

--- scripts/blog.py
+++ scripts/blog.py
@@ -166,7 +166,9 @@ HTML_HEADER: str = f"""<head>
 max-video-preview:-1, max-image-preview:large"/>
     <meta name="generator" \
 content="ari-web blog generator version {__version__}"/>
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link
         rel="stylesheet"
         href="/content/styles.css"
